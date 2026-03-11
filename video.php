da754adbbb19698a96ab6ed11db7c9eaf51363e8
Finn740 <6055458@mborijnland.nl>
2026-03-11 13:24:29 +0100

    Fix CSS file reference case sensitivity and center align list items

--- video.php
+++ video.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Video Detail Page</title>
-    <link rel="stylesheet" href="Style/Video.css">
+    <link rel="stylesheet" href="Style/video.css">
 </head>
 <body>
 
